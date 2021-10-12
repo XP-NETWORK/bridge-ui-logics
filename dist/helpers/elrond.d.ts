d157369148003bdad5593170389f2fb31906f8b7
rupansh <rupanshsekar@hotmail.com>
2021-10-12 09:41:27 +0000

    Build: (2daef86) elrond: mintable esdts

--- dist/helpers/elrond.d.ts
+++ dist/helpers/elrond.d.ts
@@ -101,7 +101,9 @@ declare type EventIdent = number;
 /**
  * Traits implemented by this module
  */
-export declare type ElrondHelper = BalanceCheck<string | Address, BigNumber> & BatchWrappedBalanceCheck<string | Address, BigNumber> & TransferForeign<ISigner, string, EasyBalance, Transaction, EventIdent> & UnfreezeForeign<ISigner, string, EasyBalance, Transaction, EventIdent> & TransferNftForeign<ISigner, string, EasyBalance, NftInfo, Transaction, EventIdent> & UnfreezeForeignNft<ISigner, string, EasyBalance, number, Transaction, EventIdent> & IssueESDTNFT & MintNft<ISigner, NftIssueArgs, void> & ListNft<string, string, EsdtNftInfo> & GetLockedNft<NftInfo, EsdtNftInfo> & DecodeWrappedNft<EsdtNftInfo> & DecodeRawNft;
+export declare type ElrondHelper = BalanceCheck<string | Address, BigNumber> & BatchWrappedBalanceCheck<string | Address, BigNumber> & TransferForeign<ISigner, string, EasyBalance, Transaction, EventIdent> & UnfreezeForeign<ISigner, string, EasyBalance, Transaction, EventIdent> & TransferNftForeign<ISigner, string, EasyBalance, NftInfo, Transaction, EventIdent> & UnfreezeForeignNft<ISigner, string, EasyBalance, number, Transaction, EventIdent> & IssueESDTNFT & MintNft<ISigner, NftIssueArgs, void> & ListNft<string, string, EsdtNftInfo> & GetLockedNft<NftInfo, EsdtNftInfo> & DecodeWrappedNft<EsdtNftInfo> & DecodeRawNft & {
+    mintableEsdts(address: Address): Promise<string[]>;
+};
 /**
  * Create an object implementing cross chain utilities for elrond
  *
